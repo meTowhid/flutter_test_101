c51cc1f150fce46601152624247c31e58c636d36
towhid <bt.towhid@gmail.com>
2024-03-05 20:20:53 +0600

    initial project creation with very_good_venture

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flutter_task_101
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_task_101/app/app.dart';
+import 'package:flutter_task_101/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
